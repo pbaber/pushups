7c99121d50f6fe3db87efb34404c1a447ba60c8b
Paul <ynom@pm.me>
2025-10-10 12:15:12 -0700

    See todays total pushups

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use chrono::{self, Local};
+use chrono::{self, Local, TimeZone};
 use clap::Parser;
 use rusqlite::{Connection, params};
 
 #[derive(Parser)]
 #[command(name = "pushups")]
 #[command(about = "Track your pushups", long_about = None)]
-struct Cli {
-    reps: u32,
+enum Cli {
+    /// add number of pushups for a set
+    Add {
+        reps: u32,
+    },
+    Today,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,8 +19,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     make_database()?;
     let conn = Connection::open("./pushups.db")?;
 
-    add_pushups(&conn, cli.reps)?;
-    println!("Added {} pushups", cli.reps);
+    match cli {
+        Cli::Add { reps } => {
+            add_pushups(&conn, reps)?;
+            println!("Added {reps} pushups");
+        }
+        Cli::Today => {
+            let todays_pushups = todays_pushups(&conn)?;
+            println!("We've done {todays_pushups} pushups today");
+        }
+    }
 
     Ok(())
 }
@@ -56,4 +68,24 @@ fn add_pushups(conn: &Connection, reps: u32) -> Result<(), rusqlite::Error> {
     )?;
 
     Ok(())
+}
+
+fn days_pushups(conn: &Connection, time: chrono::DateTime<Local>) -> Result<u32, rusqlite::Error> {
+    let start = time.date_naive().and_hms_opt(0, 0, 0).unwrap();
+    let start_local = Local.from_local_datetime(&start).unwrap();
+
+    let end = start + chrono::Duration::days(1);
+    let end_local = Local.from_local_datetime(&end).unwrap();
+
+    let total: Option<i64> = conn.query_row(
+        "SELECT SUM(reps) FROM pushups WHERE timestamp >= ?1 AND timestamp < ?2",
+        params![start_local.to_rfc3339(), end_local.to_rfc3339()],
+        |row| row.get(0),
+    )?;
+
+    Ok(total.unwrap_or(0) as u32)
+}
+
+fn todays_pushups(conn: &Connection) -> Result<u32, rusqlite::Error> {
+    days_pushups(conn, Local::now())
 }
